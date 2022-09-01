1cd77512688bcca0a85b609d257e758045f70e4b
Benjamin Weigang <benjamin.weigang@mailbox.org>
2022-09-01 17:07:04 +0200

    Deploy by building a wheel and installing it

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 PI_LOC=ben@192.168.178.41
+WHEEL_FILE=ai_image_frame-0.1.0-py3-none-any.whl
 
-scp -r src/ai_image_frame/ $PI_LOC:
-scp -r images/ $PI_LOC:ai_image_frame/
+# Copy images
+ssh $PI_LOC mkdir -p ai_image_frame
+scp -r images $PI_LOC:ai_image_frame/
+
+# Install the python package
+poetry build
+scp dist/$WHEEL_FILE $PI_LOC:ai_image_frame/
+ssh $PI_LOC pip install ai_image_frame/$WHEEL_FILE
+ssh $PI_LOC pip install ai_image_frame/$WHEEL_FILE --force-reinstall --no-deps
+ssh $PI_LOC rm ai_image_frame/$WHEEL_FILE
